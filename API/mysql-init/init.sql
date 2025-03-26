eeb89631996d06837c720ce678ad44089315713e
PedroPsousilva <pedro.soupsilva@gmail.com>
2025-03-26 16:24:42 -0300

    26-03

--- API/mysql-init/init.sql
+++ API/mysql-init/init.sql
@@ -226,34 +226,6 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'vio_pedro'
 --
-/*!50003 DROP PROCEDURE IF EXISTS `registrar_compra` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `registrar_compra`(
-in p_id_usuario int,
-in p_id_ingresso int,
-in p_id_quantidade int
-)
-begin
-declare v_id_compra int;
-insert into compra (data_compra, fk_id_usuario)
-values(now(), p_id_usuario);
-set v_id_compra = last_insert_id();
-insert into ingresso_compra (fk_id_compra, fk_id_ingresso, quantidade)
-values (v_id_compra, p_id_ingresso, p_id_quantidade);
-end ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -264,4 +236,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-24 16:18:38
+-- Dump completed on 2025-03-26 16:22:59
